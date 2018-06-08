799a45e7821fdad7dd4e406ae03f04506982b0a0
Jonathan <jonathan.frere@gmail.com>
2018-06-08 10:35:10 +0200

    Parse dice with a parser combinator
    There are way too many parsing options out there...

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,15 @@
-export interface Dice {
+type WithKind<T, Kind extends string> = T & { kind: Kind };
+
+interface DiceAttrs {
     diceSides: number[];
     noDice: number;
+}
+
+export type Dice = WithKind<DiceAttrs, 'dice'>
+
+export function dice(dice: DiceAttrs): Dice {
+    return {
+        kind: 'dice',
+        ...dice
+    }
 }
